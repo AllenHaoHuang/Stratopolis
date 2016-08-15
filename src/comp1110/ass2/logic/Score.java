173aab93266361ad2bb4126b6897bf64362cd7ee
yjl427 <u5894100@anu.edu.au>
2016-08-15 19:41:20 +1000

    Marvin: done the score.

--- src/comp1110/ass2/logic/Score.java
+++ src/comp1110/ass2/logic/Score.java
@@ -6,9 +6,33 @@ package comp1110.ass2.logic;
 
 public class Score {
     // FIXME Marvin to do this: Logic for evaluating score of board
+    private String board;
+    //check whether the game is end
+    public boolean checkEnd () {
+        return true;
+    }
+
+    //find the number of the largest area achieved by either red or green
+    public int getLargestArea (String board) {
+       return 0;
+    }
+
+    public int getHeight (String board){
+        return 0;
+    }
+
+    public int Score (String board) {
+        int a = getLargestArea(board);
+        int b = getHeight(board);
+        return  a*b;
+    }
+
+}
+
+
 
     /* We basically take in a board (which is a string), and do all the fancy logic
      * to calculate the score.
      * We know the colour of the shapes from the 'Tile' enum class, anything across the package is accessible
      */
-}
+
