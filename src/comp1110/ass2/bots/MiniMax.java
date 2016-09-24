13b6263ccb6d743f7eb02ed38ac695dd8f5cf3c5
Allen <allenhuang307@gmail.com>
2016-09-24 21:50:24 +1000

    attempted alpha beta

--- src/comp1110/ass2/bots/MiniMax.java
+++ src/comp1110/ass2/bots/MiniMax.java
@@ -5,6 +5,7 @@ import comp1110.ass2.logic.Colour;
 import comp1110.ass2.logic.Shape;
 import comp1110.ass2.logic.Tile;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 class MiniMax {
@@ -62,6 +63,86 @@ class MiniMax {
                 }
             }
             return minScore;
+        }
+    }
+
+    static int mimiMaxAlphaBeta(BoardState node, int depth, Colour myPlayer, boolean isMax, int alpha, int beta, LinkedList<Tile> moveList) {
+        if (depth == 0 || node.isFinished()) {
+            return node.getScore(myPlayer) - node.getScore(myPlayer.nextPlayer());
+        }
+
+        if (moveList.isEmpty()) {
+            if (node.getPlayerTurn() == myPlayer) {
+                return alpha;
+            } else {
+                return beta;
+            }
+        }
+
+        if (isMax) {
+            // Shape shape;
+            if (myPlayer.isGreen()) {
+            //    shape = node.getGreenShapes().getFirst();
+                node.removeGreenShape();
+            } else {
+            //    shape = node.getRedShapes().getFirst();
+                node.removeRedShape();
+            }
+
+            Tile head = moveList.getFirst();
+            LinkedList<Tile> restOfList = moveList;
+            restOfList.removeFirst();
+            BoardState child = node;
+            child.addTile(head);
+
+            Shape nextShape;
+            if (myPlayer.isGreen()) {
+                nextShape = node.getRedShapes().getFirst();
+            } else {
+                nextShape = node.getGreenShapes().getFirst();
+            }
+
+            int newValue = mimiMaxAlphaBeta(child, depth - 1, myPlayer, false, alpha, beta, child.generatePossibleMoves(nextShape));
+
+            if (newValue >= beta) {
+                return beta;
+            } else if (newValue > alpha) {
+                return mimiMaxAlphaBeta(node, depth - 1, myPlayer, true, newValue, beta, restOfList);
+            } else {
+                return mimiMaxAlphaBeta(node, depth - 1, myPlayer, true, alpha, beta, restOfList);
+            }
+        } else {
+            // Shape shape;
+            if (myPlayer.isGreen()) {
+            //    shape = node.getRedShapes().getFirst();
+                node.removeRedShape();
+            } else {
+            //    shape = node.getGreenShapes().getFirst();
+                node.removeGreenShape();
+            }
+
+            Tile head = moveList.getFirst();
+            LinkedList<Tile> restOfList = moveList;
+            restOfList.removeFirst();
+            BoardState child = node;
+            child.addTile(head);
+
+            Shape nextShape;
+            if (myPlayer.isGreen()) {
+                nextShape = node.getGreenShapes().getFirst();
+            } else {
+                nextShape = node.getRedShapes().getFirst();
+            }
+
+            int newValue = mimiMaxAlphaBeta(child, depth - 1, myPlayer, false, alpha, beta, child.generatePossibleMoves(nextShape));
+
+            if (newValue <= alpha) {
+                return alpha;
+            } else if (newValue < beta) {
+                return mimiMaxAlphaBeta(node, depth - 1, myPlayer, true, alpha, newValue, restOfList);
+            } else {
+                return mimiMaxAlphaBeta(node, depth - 1, myPlayer, true, alpha, beta, restOfList);
+            }
         }
     }
 }
